4c94651a80d31e4a3a90d76fb2a857cf6fbf142a
Gustavo Guedes <gugaguedes11@gmail.com>
2025-10-15 19:48:19 -0300

    Implementado createUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,24 @@
-import { Param, Get, Controller } from '@nestjs/common';
+import { Param, Get, Controller, Post, Body } from '@nestjs/common';
 import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+    constructor(
+        private readonly ConfigService: ConfigService,
+        private readonly userService: UserService
+    ) {}
+
+
     @Get(':id')
     findOne(@Param('id', CustomParseIntPipe) id: string) {
         return `Ola do controller do user #${id}`
+    }
+
+    @Post()
+    create(@Body() dto: CreateUserDto) {
+        return this.userService.create(dto)
     }
 }
