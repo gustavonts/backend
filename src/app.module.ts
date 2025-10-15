5abbd9cd687c58e6c5d1c92820c13b48532ee7c9
Gustavo Guedes <gugaguedes11@gmail.com>
2025-10-14 22:30:40 -0300

    implementada conexão com banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CarModule } from './car/car.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UserModule, CarModule],
+  imports: [AuthModule, UserModule, CarModule, ConfigModule.forRoot({
+    isGlobal: true
+  }),
+  TypeOrmModule.forRoot({
+    type: process.env.DB_TYPE as 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT ?? '5432', 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: process.env.DB_SYNCHRONIZE === '1',
+    autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1'
+  })
+],
   controllers: [AppController],
   providers: [AppService],
 })
